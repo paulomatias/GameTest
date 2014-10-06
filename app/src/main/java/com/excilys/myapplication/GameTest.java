155e3812d7dd0af4960da0b1b108f081ac58fa1f
Jean Herrier <jherrier@excilys.com>
2014-10-06 15:27:49 +0200

    Texture d'herbe et click sur le plateau

--- app/src/main/java/com/excilys/myapplication/GameTest.java
+++ app/src/main/java/com/excilys/myapplication/GameTest.java
@@ -1,9 +1,12 @@
 package com.excilys.myapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -19,16 +22,22 @@ public class GameTest extends Activity {
         TableLayout tlGameBoard = (TableLayout) findViewById(R.id.tl_gameBoard);
         LinearLayout llGameBoard = (LinearLayout) findViewById(R.id.ll_gameBoard);
         Drawable drawable = getResources().getDrawable(R.drawable.textview_border);
-        TableRow.LayoutParams params = new TableRow.LayoutParams(50, 50);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(64, 64);
 
         for (int i = 0; i < 10; i++) {
             TableRow tableRow = new TableRow(this);
             for (int j = 0; j < 10; j++) {
-                TextView textView = new TextView(this);
+                final TextView textView = new TextView(this);
                 textView.setLayoutParams(params);
-                textView.setText("" + i + "" + j);
+                textView.setText("");
                 textView.setGravity(Gravity.CENTER);
-                textView.setBackground(drawable);
+                textView.setBackgroundResource(R.drawable.grass);
+                textView.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       textView.setBackgroundColor(Color.BLUE);
+                    }
+                });
                 tableRow.addView(textView);
             }
             tlGameBoard.addView(tableRow);
