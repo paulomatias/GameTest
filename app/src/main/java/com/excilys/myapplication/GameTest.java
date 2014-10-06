a0698bf83c8a31ea8ce933ad96c7149bd29dd8e5
Jean Herrier <jherrier@excilys.com>
2014-10-06 17:18:51 +0200

    StringHelper, demo map et ressources

--- app/src/main/java/com/excilys/myapplication/GameTest.java
+++ app/src/main/java/com/excilys/myapplication/GameTest.java
@@ -1,16 +1,19 @@
 package com.excilys.myapplication;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import helper.StringHelper;
+
 
 public class GameTest extends Activity {
 
@@ -19,9 +22,12 @@ public class GameTest extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gametest);
 
+        String mapString = StringHelper.convertStreamToString(getResources().openRawResource(R.raw.tilemap01));
+        Log.i(this.getClass().toString(), "mapString : " + mapString);
+        String[] map = mapString.split("\\+");
+
         TableLayout tlGameBoard = (TableLayout) findViewById(R.id.tl_gameBoard);
         LinearLayout llGameBoard = (LinearLayout) findViewById(R.id.ll_gameBoard);
-        Drawable drawable = getResources().getDrawable(R.drawable.textview_border);
         TableRow.LayoutParams params = new TableRow.LayoutParams(64, 64);
 
         for (int i = 0; i < 10; i++) {
@@ -31,19 +37,38 @@ public class GameTest extends Activity {
                 textView.setLayoutParams(params);
                 textView.setText("");
                 textView.setGravity(Gravity.CENTER);
-                textView.setBackgroundResource(R.drawable.grass);
-                textView.setOnClickListener( new View.OnClickListener() {
+                textView.setBackground(switchTile(Integer.parseInt(map[i].substring(j, j + 1))));
+
+                textView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                       textView.setBackgroundColor(Color.BLUE);
+                        textView.setBackgroundColor(Color.BLUE);
                     }
                 });
                 tableRow.addView(textView);
             }
             tlGameBoard.addView(tableRow);
             llGameBoard.setGravity(Gravity.CENTER);
+            llGameBoard.setBackground(getResources().getDrawable(R.drawable.grass_background));
         }
         tlGameBoard.setRotation(45);
         tlGameBoard.setRotationX(10);
+    }
+
+    private Drawable switchTile(int tileNumber) {
+
+        switch (tileNumber) {
+            case 0:
+                return getResources().getDrawable(R.drawable.grass);
+            case 1:
+                return getResources().getDrawable(R.drawable.rock);
+            case 2:
+                return getResources().getDrawable(R.drawable.stone);
+            case 3:
+                return getResources().getDrawable(R.drawable.water);
+            default:
+                break;
+        }
+        return null;
     }
 }
---
+++ app/src/main/java/helper/StringHelper.java
@@ -0,0 +1,35 @@
+package helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringHelper {
+
+    public static String convertStreamToString(java.io.InputStream is) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
