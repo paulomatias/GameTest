db9413834b9e2422959682e5898ba3000f72ff9e
Jean Herrier <jherrier@excilys.com>
2014-10-07 17:42:50 +0200

    Scroll size auto from tilemap

--- app/src/main/java/com/excilys/myapplication/GameTest.java
+++ app/src/main/java/com/excilys/myapplication/GameTest.java
@@ -1,21 +1,21 @@
 package com.excilys.myapplication;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import helper.StringHelper;
-
 
 public class GameTest extends Activity {
 
@@ -28,29 +28,48 @@ public class GameTest extends Activity {
     private VScroll vScroll;
     private float scaleFactor = 1.0f;
     private TextView currentTextView;
+    private int maxLengthX = 0;
+    private int maxLengthY = 0;
+    private Resources resources;
 
+    // Drawable pour la map
+    private Drawable dGrass;
+    private Drawable dStone;
+    private Drawable dWater;
+    private Drawable dRock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gametest);
 
+        resources = getResources();
+        dGrass = resources.getDrawable(R.drawable.grass);
+        dStone = resources.getDrawable(R.drawable.stone);
+        dWater = resources.getDrawable(R.drawable.water);
+        dRock = resources.getDrawable(R.drawable.rock);
+
+        //Découpage des Tilemap
         String mapString = StringHelper.convertStreamToString(getResources().openRawResource(R.raw.tilemap01));
-        Log.i(this.getClass().toString(), "mapString : " + mapString);
         String[] map = mapString.split("\\+");
 
         TableLayout tlGameBoard = (TableLayout) findViewById(R.id.tl_gameBoard);
-        LinearLayout llGameBoard = (LinearLayout) findViewById(R.id.ll_gameBoard);
+        RelativeLayout rlGameBoard = (RelativeLayout) findViewById(R.id.rl_gameBoard);
         hScroll = (HScroll) findViewById(R.id.hs_gameBoard);
         vScroll = (VScroll) findViewById(R.id.vs_gameBoard);
         Drawable drawable = getResources().getDrawable(R.drawable.textview_border);
 
-
         scaleGestureDetector = new ScaleGestureDetector(getApplicationContext(), new ScaleListener());
         TableRow.LayoutParams params = new TableRow.LayoutParams(tileSize, tileSize);
+        maxLengthY = map.length;
+
+        // Population du tableau représentant la carte
         for (int i = 0; i < map.length; i++) {
             TableRow tableRow = new TableRow(this);
             for (int j = 0; j < map[i].length(); j++) {
+                if (map[i].length() > maxLengthX) {
+                    maxLengthX = map[i].length();
+                }
                 final TextView textView = new TextView(this);
                 textView.setLayoutParams(params);
                 textView.setText("");
@@ -62,22 +81,53 @@ public class GameTest extends Activity {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
                         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
                             mx = event.getRawX();
                             my = event.getRawY();
                             currentTextView = textView;
-
                             return true;
                         }
                         return false;
                     }
                 });
+                // Désactivation de l'accélération matérielle pour les TextViews
+                textView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
                 tableRow.addView(textView);
             }
             tlGameBoard.addView(tableRow);
-            llGameBoard.setGravity(Gravity.CENTER);
-            llGameBoard.setBackground(getResources().getDrawable(R.drawable.grass_background));
+            rlGameBoard.setBackground(resources.getDrawable(R.drawable.grass_background));
         }
+
+        // Initialisation et positionnement du GameBoard
+        initGameBoardPosition(tlGameBoard);
+    }
+
+    private void initGameBoardPosition(TableLayout tlGameBoard) {
+        float offsetTop;
+        float offsetLeft;
+        if (maxLengthX > maxLengthY) {
+            offsetTop = (float) -(Math.sqrt(2) / 4 * tileSize * (-maxLengthX + maxLengthY) - maxLengthY * tileSize / 4);
+            offsetLeft = (float) -(Math.sqrt(2) / 4 * tileSize * (-maxLengthX - maxLengthY) + maxLengthX * tileSize / 2);
+        } else {
+            offsetTop = (float) (Math.sqrt(2) / 4 * tileSize * (-maxLengthX + maxLengthY) - maxLengthY * tileSize / 4);
+            offsetLeft = (float) (Math.sqrt(2) / 4 * tileSize * (-maxLengthX - maxLengthY) + maxLengthX * tileSize / 2);
+        }
+
+        int heightScroll = (int) (maxLengthY * tileSize + offsetTop * 2);
+        int widthScroll = (int) (maxLengthX * tileSize + offsetLeft * 2);
+
+        hScroll.setMinimumHeight(heightScroll);
+        vScroll.setMinimumHeight(heightScroll);
+
+        hScroll.setMinimumWidth(widthScroll);
+        vScroll.setMinimumWidth(widthScroll);
+
+        FrameLayout.LayoutParams vPArams = new VScroll.LayoutParams(widthScroll, heightScroll);
+        hScroll.setLayoutParams(vPArams);
+
+        tlGameBoard.setTranslationX(offsetLeft);
+        tlGameBoard.setTranslationY(offsetTop);
+        hScroll.smoothScrollTo(widthScroll / 2, 0);
+        vScroll.smoothScrollTo(0, heightScroll / 2);
         tlGameBoard.setRotation(45);
     }
 
@@ -111,8 +161,6 @@ public class GameTest extends Activity {
                     if (!isZoomed) {
                         curX = event.getX();
                         curY = event.getY();
-
-
                         if (!moved && currentTextView != null) {
                             //Traitement lors du click sur une case
                             currentTextView.setBackgroundColor(Color.BLUE);
@@ -121,7 +169,6 @@ public class GameTest extends Activity {
                             vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
                             hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
                         }
-
                         mx = event.getX();
                         my = event.getY();
                         moved = false;
@@ -137,13 +184,13 @@ public class GameTest extends Activity {
 
         switch (tileNumber) {
             case 0:
-                return getResources().getDrawable(R.drawable.grass);
+                return dGrass;
             case 1:
-                return getResources().getDrawable(R.drawable.rock);
+                return dRock;
             case 2:
-                return getResources().getDrawable(R.drawable.stone);
+                return dStone;
             case 3:
-                return getResources().getDrawable(R.drawable.water);
+                return dWater;
             default:
                 break;
         }
